21f87f34f6f0ae5517c16001c0ef5de1ea5b4f27
Matt Zanchelli <matt@mdznr.com>
2014-04-03 10:48:00 -0400

    Initial Commit

---
+++ Matt Zanchelli/MTZViewController.m
@@ -0,0 +1,29 @@
+//
+//  MTZViewController.m
+//  Matt Zanchelli
+//
+//  Created by Matt Zanchelli on 4/3/14.
+//  Copyright (c) 2014 Matt Zanchelli. All rights reserved.
+//
+
+#import "MTZViewController.h"
+
+@interface MTZViewController ()
+
+@end
+
+@implementation MTZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Matt ZanchelliTests/Matt_ZanchelliTests.m
@@ -0,0 +1,34 @@
+//
+//  Matt_ZanchelliTests.m
+//  Matt ZanchelliTests
+//
+//  Created by Matt Zanchelli on 4/3/14.
+//  Copyright (c) 2014 Matt Zanchelli. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Matt_ZanchelliTests : XCTestCase
+
+@end
+
+@implementation Matt_ZanchelliTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
