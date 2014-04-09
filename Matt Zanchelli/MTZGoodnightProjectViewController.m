eb2a214a974b4e3cb2f5a361f48e45a9ce92a406
Matt Zanchelli <matt@mdznr.com>
2014-04-08 23:23:07 -0400

    Header view sticks to the top of the scrollview when scrolling in negative y territory.

--- Matt Zanchelli/MTZGoodnightProjectViewController.m
+++ Matt Zanchelli/MTZGoodnightProjectViewController.m
@@ -10,32 +10,14 @@
 
 @interface MTZGoodnightProjectViewController ()
 
-@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-@property (weak, nonatomic) IBOutlet UIView *contentView;
-
 @end
 
 @implementation MTZGoodnightProjectViewController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
