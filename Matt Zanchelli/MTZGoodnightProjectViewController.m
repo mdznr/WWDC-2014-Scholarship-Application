aa5a899ff1288820379bf8ff3c7f040c4262cda6
Matt Zanchelli <matt@mdznr.com>
2014-04-14 19:08:34 -0400

    Reordered content in Goodnight section to match that of the flow of using the app.

--- Matt Zanchelli/MTZGoodnightProjectViewController.m
+++ Matt Zanchelli/MTZGoodnightProjectViewController.m
@@ -15,11 +15,11 @@
 @property (weak, nonatomic) IBOutlet UILabel *goodnight;
 @property (weak, nonatomic) IBOutlet UILabel *description;
 
-@property (weak, nonatomic) IBOutlet UILabel *restEasy;
-@property (weak, nonatomic) IBOutlet UILabel *restEasy2;
-
 @property (weak, nonatomic) IBOutlet UILabel *haveAGoodNight;
 @property (weak, nonatomic) IBOutlet UILabel *haveAGoodNight2;
+
+@property (weak, nonatomic) IBOutlet UILabel *restEasy;
+@property (weak, nonatomic) IBOutlet UILabel *restEasy2;
 
 @property (weak, nonatomic) IBOutlet UILabel *goodMorning;
 @property (weak, nonatomic) IBOutlet UILabel *goodMorning2;
@@ -36,12 +36,12 @@
 	[self.goodnight applyMTZStyle:MTZStyleProjectTitle];
 	[self.description applyMTZStyle:MTZStyleProjectDescription];
 	
-	[self.restEasy applyMTZStyle:MTZStyleGoodnightSectionTitle];
 	[self.haveAGoodNight applyMTZStyle:MTZStyleGoodnightSectionTitle];
+	[self.restEasy applyMTZStyle:MTZStyleGoodnightSectionTitle];
 	[self.goodMorning applyMTZStyle:MTZStyleGoodnightSectionTitle];
 	
-	[self.restEasy2 applyMTZStyle:MTZStyleGoodnightSectionSubtitle];
 	[self.haveAGoodNight2 applyMTZStyle:MTZStyleGoodnightSectionSubtitle];
+	[self.restEasy2 applyMTZStyle:MTZStyleGoodnightSectionSubtitle];
 	[self.goodMorning2 applyMTZStyle:MTZStyleGoodnightSectionSubtitle];
 }
 
@@ -54,21 +54,52 @@
 	
 	NSUInteger page = round(scrollView.contentOffset.y / scrollView.frame.size.height);
 	switch (page) {
-		case 0:
-			self.screenshot.image = [UIImage imageNamed:@"Goodnight-Time Set"];
-			break;
-		case 1:
-			self.screenshot.image = [UIImage imageNamed:@"Goodnight-Alarm"];
-			break;
-		case 2:
-			self.screenshot.image = [UIImage imageNamed:@"Goodnight-Wake Times"];
-			break;
-		case 3:
-			self.screenshot.image = [UIImage imageNamed:@"Goodnight-Good Morning"];
-			break;
-		default:
-			self.screenshot.image = nil;
-			break;
+		case 0: {
+			[UIView animateWithDuration:0.4f
+								  delay:0.0f
+								options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseInOut
+							 animations:^{
+								 self.screenshot.image = [UIImage imageNamed:@"Goodnight-Time Set"];
+							 }
+							 completion:nil];
+		} break;
+		case 1: {
+			[UIView animateWithDuration:0.4f
+								  delay:0.0f
+								options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseInOut
+							 animations:^{
+								 self.screenshot.image = [UIImage imageNamed:@"Goodnight-Wake Times"];
+							 }
+							 completion:nil];
+		} break;
+		case 2: {
+			[UIView animateWithDuration:0.4f
+								  delay:0.0f
+								options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseInOut
+							 animations:^{
+								 self.screenshot.image = [UIImage imageNamed:@"Goodnight-Alarm"];
+							 }
+							 completion:nil];
+
+		} break;
+		case 3: {
+			[UIView animateWithDuration:0.4f
+								  delay:0.0f
+								options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseInOut
+							 animations:^{
+								 self.screenshot.image = [UIImage imageNamed:@"Goodnight-Good Morning"];
+							 }
+							 completion:nil];
+		} break;
+		default: {
+			[UIView animateWithDuration:0.4f
+								  delay:0.0f
+								options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseInOut
+							 animations:^{
+								 self.screenshot.image = nil;
+							 }
+							 completion:nil];
+		} break;
 	}
 }
 
